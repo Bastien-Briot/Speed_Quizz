1f8b842d28137152f10c6fd863a4fcb9f99b774a
briobas <bastien.briot@divtec.ch>
2022-02-08 10:18:51 +0100

    Page principal xml finit

--- app/src/main/java/com/briobas/speed_quizz/MainActivity.java
+++ app/src/main/java/com/briobas/speed_quizz/MainActivity.java
@@ -1,8 +1,20 @@
 package com.briobas.speed_quizz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button BTN_Ajouter = findViewById(R.id.BTN_Add);
+        Button BTN_Commencer = findViewById(R.id.BTN_Commencer);
+        EditText Player1 = findViewById(R.id.main_text_player1);
+        EditText Player2 = findViewById(R.id.main_text_player2);
+
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+
+        BTN_Ajouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Player1.setVisibility(View.VISIBLE);
+            }
+        });
+        Player1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                Player2.setVisibility(View.VISIBLE);
+            }
+        });
+        Player2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                BTN_Commencer.setVisibility(View.VISIBLE);
+            }
+        });
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_favorite:
+                break;
+            case R.id.action_settings:
+                break;
+            case R.id.action_question:
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
